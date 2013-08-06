f7ca97e495d564a9c99a54c737aad8a26d5a4f95
Andrew Warfield <hiasant@gmail.com>
2013-08-05 18:36:54 -0700

    BoardConifig: use 4.8 as target toolchain

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2013 The Ultimatum Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -40,6 +41,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_VARIANT := krait
 TARGET_CPU_SMP := true
+TARGET_GCC_VERSION := 4.8
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
